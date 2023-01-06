cf5d03c3a8e6fa958b37d603f8a373e4e28edd13
zqwz <zqwz@naver.com>
2023-01-06 23:24:33 +0900

    feat(Order): 할인되는 금액 출력 기능 추가

--- src/app/operator/Order.java
+++ src/app/operator/Order.java
@@ -3,7 +3,6 @@ package app.operator;
 import app.discount.Discount;
 
 public class Order {
-
     private Cart cart;
     private Discount discount;
 
@@ -16,11 +15,11 @@ public class Order {
         discount.checkAllDiscountCondition();
         int totalPrice = cart.calculateTotalPrice();
         int finalDiscount = discount.discounted(totalPrice);
-
         System.out.println("[\uD83D\uDCE3] 주문이 완료되었습니다.");
         System.out.println("[\uD83D\uDCE3] 주문 내역은 다음과 같습니다.");
         cart.printCartDetail();
         System.out.println("-".repeat(60));
-        System.out.printf("금액 합계      : %d원\n", finalDiscount);
+        System.out.printf(" 할인 금액      : %d원\n", totalPrice - finalDiscount);
+        System.out.printf(" 결제 금액      : %d원\n", finalDiscount);
     }
 }
