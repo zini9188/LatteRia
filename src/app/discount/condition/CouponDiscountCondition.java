06b54277f8624c0b796fdb5157a5c024110a06a0
zqwz <zqwz@naver.com>
2023-01-06 23:26:31 +0900

    refactor(DiscountCondition): scanner 제거, line 정리

--- src/app/discount/condition/CouponDiscountCondition.java
+++ src/app/discount/condition/CouponDiscountCondition.java
@@ -1,31 +1,26 @@
 package app.discount.condition;
 
+import app.Reader;
 import app.discount.policy.DiscountPolicy;
 
-import java.util.Scanner;
-
-public class CouponDiscountCondition implements DiscountCondition{
-
-    boolean isSatisfied;
-
-    DiscountPolicy discountPolicy;
+public class CouponDiscountCondition implements DiscountCondition {
+    private boolean isSatisfied;
+    private DiscountPolicy discountPolicy;
 
     public CouponDiscountCondition(DiscountPolicy discountPolicy) {
         this.discountPolicy = discountPolicy;
     }
 
-    public void setSatisfied(boolean satisfied) {
+    private void setSatisfied(boolean satisfied) {
         isSatisfied = satisfied;
     }
 
     @Override
     public void checkDiscountCondition() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("할인 쿠폰을 보유중 입니까? (1)_예 (2)_아니오");
-        String input = scanner.nextLine();
-        if (input.equals("1")) setSatisfied(true);
-        else if (input.equals("2")) setSatisfied(false);
+        System.out.print("%d원 할인 쿠폰을 보유중 입니까? (1)_예 (2)_아니오");
+        int input = Reader.readDiscount();
+        if (input == 1) setSatisfied(true);
+        else if (input == 2) setSatisfied(false);
     }
 
     @Override
--- src/app/discount/condition/KidDiscountCondition.java
+++ src/app/discount/condition/KidDiscountCondition.java
@@ -1,30 +1,26 @@
 package app.discount.condition;
 
+import app.Reader;
 import app.discount.policy.DiscountPolicy;
 
-import java.util.Scanner;
-
-public class KidDiscountCondition implements DiscountCondition{
-    boolean isSatisfied;
-
-    DiscountPolicy discountPolicy;
+public class KidDiscountCondition implements DiscountCondition {
+    private boolean isSatisfied;
+    private DiscountPolicy discountPolicy;
 
     public KidDiscountCondition(DiscountPolicy discountPolicy) {
         this.discountPolicy = discountPolicy;
     }
 
-    public void setSatisfied(boolean satisfied) {
+    private void setSatisfied(boolean satisfied) {
         isSatisfied = satisfied;
     }
 
     @Override
     public void checkDiscountCondition() {
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("20세 미만입니까? (1)_예 (2)_아니오");
-        String input = scanner.nextLine();
-        if (input.equals("1")) setSatisfied(true);
-        else if (input.equals("2")) setSatisfied(false);
+        int input = Reader.readDiscount();
+        if (input == 1) setSatisfied(true);
+        else if (input == 2) setSatisfied(false);
     }
 
     @Override
